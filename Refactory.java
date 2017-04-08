34e623d1184b819af70ff5bdc45bd4a1e28c5c78
tipu27 <tipusultan5357@gmail.com>
2017-04-09 02:00:22 +0600

    Second commit

--- Refactory.java
+++ Refactory.java
@@ -4,7 +4,38 @@ public class RefactoryDemo {
 
 	private MyCollection _orders = new MyCollection();
 	private String _name = "Customer Name";
+	void printOwing(){package RefactoryDemoPacage;
+
+public class RefactoryDemo {
+
+	private MyCollection _orders = new MyCollection();
+	private String _name = "Customer Name";
 	void printOwing(){
+		Enumeration e = _orders.elements();
+		double outstanding = 0.0;
+		int count=0;
+		//print banner
+		printBanner();
+
+		//calculate outstanding
+		while (e.hasMoreElements()){
+			Order each = (Order) e.nextElement();
+			outstanding += each.getAmount();
+			count++;
+		}
+
+		// print details
+		System.out.println("name: " + _name);
+		Syatem.out.println("amount: " + outstanding);
+		System.out.println("Count: " + count)
+	}
+	private void printBanner() {
+		System.out.println("*************************");
+		System.out.println("***** Customer Owes *****");
+		System.out.println("*************************");
+	}
+}
+
 		Enumeration e = _orders.elements();
 		double outstanding = 0.0;
 		int count=0;
